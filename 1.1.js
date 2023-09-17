dace29ad9a046ddcd78dfc6a47041097568f73ba
fathi <fathizidan52@gmail.com>
2023-09-17 11:46:45 +0300

    upload

---
+++ 1.1.js
@@ -0,0 +1,22 @@
+let x = true;
+console.log(typeof x);
+
+let y = 'hello'
+console.log(typeof y);
+
+let z = 345
+console.log(typeof z);
+
+let a = null;
+console.log(a);
+
+let v = undefined
+console.log(typeof v)
+
+let s = Symbol("hi");
+console.log( typeof s); 
+
+let o = Object("fathi");
+console.log(typeof o)
+
+
